c7498d0daffa9b7423a66b2bd53b6a722c0a643f
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-08-23 15:53:25 +0200

    Save

--- src/index.js
+++ src/index.js
@@ -8,40 +8,44 @@ const project = (name) => {
     return { name, tasks, addTask };
 };
 
-// const task = (title, description, dueDate, priority, checkList, notes) => {
 const task = ({title, description, dueDate, priority = 2}) => {
     const checkList = [];
     let notes;
-    function checkListItem(title) {
+    function CheckListItem(title) {
         this.title = title;
         this.isDone = false;
         this.toggle = function() {
             this.isDone = !this.isDone;
         }
     }
-    // const checkListItem = (title) => {
-    //     let isDone = false;
-    //     const toggle = () => {
-    //         console.log(this.isDone);
-    //         isDone = !isDone; // få denne til å virke
-    //         console.log(this.isDone);
-    //     };
-    //     return {title, isDone, toggle}
-    // }
     const addCheckListItem = (title) => {
-        // const item = checkListItem(title);
-        const item = new checkListItem(title);
+        const item = new CheckListItem(title);
         checkList.push(item);
     };
-    return { title, description, dueDate, priority, checkList, notes, addCheckListItem };
+    const addNotes = (text) => {
+        notes = text;
+    }
+    function getNotes() {
+        console.log(notes)
+        return this.notes;
+    }
+    return { title, description, dueDate, priority, checkList, notes, addCheckListItem, addNotes, getNotes };
 };
+
+
 let aa = task ({
     title: "Navn",
      description: "Noe som må gjøres",
-      dueDate: "2022-08-22"});
+      dueDate: "2022-08-22"
+    });
 
 console.log(aa);
 aa.addCheckListItem("Sjekk");
 console.log(aa.checkList[0]);
 aa.checkList[0].toggle();
 console.log(aa.checkList[0]);
+aa.notes = "Notater kommer her";
+// console.log(aa.notes);
+console.log(aa.getNotes());
+console.log(aa.notes)
+
