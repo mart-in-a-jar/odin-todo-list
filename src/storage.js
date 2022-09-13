db5b30d61b4a668e042f5007b6ab9f7c56796e5e
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-09-13 15:26:41 +0200

    Fix loading of checklist items

--- src/storage.js
+++ src/storage.js
@@ -18,7 +18,8 @@ function retrieveFromLocalStorage() {
                 projects[i].addTask(Object.assign(new task({title: "", description: "", dueDate: ""}), result[i].tasks[j]));
                 projects[i].tasks[j].checkList = [];
                 for(let k = 0; k < result[i].tasks[j].checkList.length; k++) {
-                    projects[i].tasks[j].addCheckListItem(result[i].tasks[j].checkList[k].title);
+                    projects[i].tasks[j].addCheckListItem("");
+                    projects[i].tasks[j].checkList[k] = Object.assign(projects[i].tasks[j].checkList[k], result[i].tasks[j].checkList[k]);
                 }
             }
         }
